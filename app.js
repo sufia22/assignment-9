bd9097feedbb7bc4650798d127ecfee66a4fec3e
Sufia <shapu23@gmail.com>
2022-04-15 00:29:06 +0600

    Upload Assignment 5

---
+++ app.js
@@ -0,0 +1,129 @@
+
+
+/**
+ * Children to Old People
+ */
+
+// let name = prompt('Name');
+// let year = prompt('Birth of Year');
+
+// console.log(ageCal(name, year));
+
+
+
+
+
+
+
+/**
+ * Area Calculation
+ */
+
+
+//  console.log(areaCal('s', 40));
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Result Calculation
+ */
+
+// let name        = prompt('Name');
+// let className   = prompt('Class Name');
+// let roll        = prompt('Roll');
+// let ban         = prompt('Bangla');
+// let eng         = prompt('English');
+// let mat         = prompt('Math');
+// let sci         = prompt('Science');
+// let ssc         = prompt('Social Science');
+// let rel         = prompt('Religion');
+
+
+
+// console.log(`
+    
+// Name        : ${name}
+// Class       : ${className}
+// Roll        : ${roll}
+// --------------------------------------------------------
+// Subject     Marks   GPA     Grade
+// Bangla       ${ban}      ${getGpa(ban)}       ${getGrade(ban)}
+// English      ${eng}      ${getGpa(eng)}       ${getGrade(eng)}
+// Math         ${mat}      ${getGpa(mat)}       ${getGrade(mat)}
+// Science      ${sci}      ${getGpa(sci)}       ${getGrade(sci)}
+// S Science    ${ssc}      ${getGpa(ssc)}       ${getGrade(ssc)}
+// Religion     ${rel}      ${getGpa(rel)}       ${getGrade(rel)}
+
+// `);
+
+
+
+
+
+
+
+
+
+
+/**
+ * Age Calculation
+ */
+
+
+// let name = prompt('Name');
+// let byear = prompt('Birth of Year');
+
+// console.log(agefunc(name, byear));
+
+
+
+
+
+
+
+ 
+/**
+ * BMI Calculation
+ */
+
+// let name   = prompt('Name');
+// let weight = prompt('Weight');
+// let height = prompt('Height');
+
+// console.log(bmiCal(name, weight, height));
+
+
+
+
+
+
+/**
+ * Currency Convert
+ */
+
+// let amount = prompt('Amount');
+// let type = prompt('Currency');
+
+
+// console.log(currencyConvert( amount, type));
+
+
+
+
+
+
+
+
+
+
+
+
+
