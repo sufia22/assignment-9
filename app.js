26ea464746c943b38b456a5686253fbfbe0ac1f3
Sufia <shapu23@gmail.com>
2022-04-27 02:53:44 +0600

    Assignment 07

--- app.js
+++ app.js
@@ -1,203 +1,473 @@
 
 
-const studentInfo = [
-    ['Safa', '3', '1', '9', 'female', 'Jatrabari', 4500],
-    ['Sadaf', '4', '2', '10', 'male', 'Uttara', 5500],
-    ['Hasib', '5', '3', '12', 'male', 'Banani', 7500],
-    ['Neha', '5', '4', '11', 'female', 'Jatrabari', 3500],
-    ['Sajib', '3', '5', '24', 'male', 'Uttara', 1500],
-    ['Nayeem', '4', '6', '21', 'male', 'Banani', 2000],
-    ['Ayesha', '3', '7', '19', 'female', 'Jatrabari', 2300],
-    ['Nahid', '5', '8', '15', 'male', 'Uttara', 8500],
-    ['Sumi', '4', '9', '14', 'female', 'Banani', 8500],
-    ['Jibon', '4', '10', '16', 'male', 'Jatrabari', 6500],
-    ['Sarmin', '3', '11', '15', 'female', 'Banani', 7000],
-    ['Polash', '5', '12', '12', 'male', 'Uttara', 2500],
-    ['Anis', '4', '13', '23', 'male', 'Jatrabari', 1500],
-    ['Habib', '3', '14', '17', 'male', 'Uttara', 5800],
-    ['Shuvo', '5', '15', '34', 'male', 'Banani', 7500],
-    ['Fahim', '4', '16', '26', 'male', 'Jatrabari', 2800],
-    ['Faiza', '3', '17', '19', 'female', 'Jatrabari', 9500],
-    ['Habib', '4', '18', '23', 'male', 'Banani', 1500],
-    ['Faysal', '5', '19', '29', 'male', 'Jatrabari', 3700],
-    ['Khokon', '3', '20', '27', 'male', 'Banani', 3600],
-    ['Krishna', '5', '21', '18', 'male', 'Uttara', 4500],
-    ['Hasan', '4', '22', '22', 'male', 'Jatrabari', 8500],
-    ['Sufia', '3', '23', '23', 'female', 'Jatrabari', 4500],
-    ['Sajol', '4', '24', '26', 'male', 'Banani', 5500],
-    ['Rabiul', '5', '25', '25', 'male', 'Uttara', 2800],
-    ['Taher', '4', '26', '28', 'male', 'Jatrabari', 3400],
-    ['Imran', '3', '27', '27', 'male', 'Banani', 1500],
-    ['Istiak', '4', '28', '18', 'male', 'Uttara', 3500],
-    ['Mahdi', '5', '29', '17', 'male', 'Jatrabari', 6700],
-    ['Masud', '3', '30', '15', 'male', 'Jatrabari', 7800],
+const students = [
+    {
+        name        : 'Zoynal Abedin',
+        roll        : 1,
+        age         : 10,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 4000
+    },
+    {
+        name        : 'Zinnah Ali',
+        roll        : 2,
+        age         : 14,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Uttara',
+        fee         : 4500
+    },
+    {
+        name        : 'Toquir Ahmed',
+        roll        : 3,
+        age         : 18,
+        classname   : 5,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 6500
+    },
+    {
+        name        : 'Tahir Hossain',
+        roll        : 4,
+        age         : 9,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 3000
+    },
+    {
+        name        : 'Sumi Alam',
+        roll        : 5,
+        age         : 15,
+        classname   : 5,
+        gender      : 'Female',
+        location    : 'Uttara',
+        fee         : 7800
+    },
+    {
+        name        : 'Shariful Islam',
+        roll        : 6,
+        age         : 23,
+        classname   : 5,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 8500
+    },
+    {
+        name        : 'Shirajam Munir',
+        roll        : 7,
+        age         : 17,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 4000
+    },
+    {
+        name        : 'Sharmin Sultana',
+        roll        : 8,
+        age         : 8,
+        classname   : 3,
+        gender      : 'Female',
+        location    : 'Uttara',
+        fee         : 8700
+    },
+    {
+        name        : 'Sha Jib',
+        roll        : 9,
+        age         : 8,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 5600
+    },
+    {
+        name        : 'Sayyid Saadi',
+        roll        : 10,
+        age         : 27,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 3400
+    },
+    {
+        name        : 'Sajol Khan',
+        roll        : 11,
+        age         : 29,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Uttara',
+        fee         : 7900
+    },
+    {
+        name        : 'Sajjadul Hossain',
+        roll        : 12,
+        age         : 30,
+        classname   : 5,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 9700
+    },
+    {
+        name        : 'S. S. M. Rayhanuzzaman',
+        roll        : 13,
+        age         : 17,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 3400
+    },
+    {
+        name        : 'Ş. Ą. Ąsha',
+        roll        : 14,
+        age         : 25,
+        classname   : 4,
+        gender      : 'Female',
+        location    : 'Uttara',
+        fee         : 6500
+    },
+    {
+        name        : 'S M Zahid Hasan',
+        roll        : 15,
+        age         : 30,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 6500
+    },
+    {
+        name        : 'S D Rubel Mahmud',
+        roll        : 16,
+        age         : 27,
+        classname   : 5,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 5800
+    },
+    {
+        name        : 'Russell Azim',
+        roll        : 17,
+        age         : 23,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Uttara',
+        fee         : 3300
+    },
+    {
+        name        : 'Rex Ekra King',
+        roll        : 18,
+        age         : 14,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 2300
+    },
+    {
+        name        : 'Raihanul Tushar',
+        roll        : 19,
+        age         : 19,
+        classname   : 5,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 4000
+    },
+    {
+        name        : 'Rafsan Jani',
+        roll        : 20,
+        age         : 29,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Uttara',
+        fee         : 6700
+    },
+    {
+        name        : 'Rabiul Zahid',
+        roll        : 21,
+        age         : 34,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 7700
+    },
+    {
+        name        : 'Pk Pk',
+        roll        : 22,
+        age         : 24,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 5500
+    },
+    {
+        name        : 'Ovimanii Sohag',
+        roll        : 23,
+        age         : 26,
+        classname   : 5,
+        gender      : 'Male',
+        location    : 'Uttara',
+        fee         : 7800
+    },
+    {
+        name        : 'Nusrat Sarmin',
+        roll        : 24,
+        age         : 15,
+        classname   : 3,
+        gender      : 'Female',
+        location    : 'Banani',
+        fee         : 6800
+    },
+    {
+        name        : 'Nasir Khan',
+        roll        : 25,
+        age         : 27,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 9000
+    },
+    {
+        name        : 'Naimur Rahman',
+        roll        : 26,
+        age         : 17,
+        classname   : 3,
+        gender      : 'Male',
+        location    : 'Uttara',
+        fee         : 7800
+    },
+    {
+        name        : 'Naimul Hasan',
+        roll        : 27,
+        age         : 26,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 5900
+    },
+    {
+        name        : 'Nahian Khondokar',
+        roll        : 28,
+        age         : 7,
+        classname   : 5,
+        gender      : 'Male',
+        location    : 'Mirpur',
+        fee         : 2000
+    },
+    {
+        name        : 'Munira Akter',
+        roll        : 29,
+        age         : 16,
+        classname   : 3,
+        gender      : 'Female',
+        location    : 'Uttara',
+        fee         : 5600
+    },
+    {
+        name        : 'Muhammad Nazmul Huda',
+        roll        : 30,
+        age         : 10,
+        classname   : 4,
+        gender      : 'Male',
+        location    : 'Banani',
+        fee         : 5400
+    }
+
 ];
 
 
+
+
+
 // Total Admnission fees
-let total_amount = 0;
-studentInfo.map( ( data, index ) => {
+// let total_fee = 0;
+// for( data of students ){
+//     console.log(`
+//         Name        : ${ data.name }
+//         Roll        : ${ data.roll }
+//         Age         : ${ data.age }
+//         Class Name  : ${ data.classname }
+//         Gender      : ${ data.gender }
+//         Location    : ${ data.location }
+//         Fee         : ${ data.fee }
+//     `);
+//     total_fee += data.fee;
+// }
 
-    console.log(`Name : ${ data[0] } - Amount : ${ data[6] }`);
-    total_amount += data[6];
-
-    
-    
-} );
-
-console.log(`
-================================
-Total Amount    =    ${total_amount}
-`);
-
-
-
-
-// All Female Students
-studentInfo.map( ( data, index ) => {
-    
-    if( data[4] == 'female' ){
-        console.log(`
-            Name     : ${ data[0] }
-            Class    : ${ data[1] }
-            Roll     : ${ data[2] }
-            Age      : ${ data[3] }
-            Gender   : ${ data[4] }
-            Location : ${ data[5] }
-            Fee      : ${ data[6] }
-        `);
-        }
-    
-} );
-
-
-
-
-
-// Students of Class 3
-studentInfo.map( ( data, index ) => {
-    if( data[1] == '3' ){
-        console.log(`
-            Name      : ${ data[0] }
-            Class     : ${ data[1] }
-            Roll      : ${ data[2] }
-            Age       : ${ data[3] }
-            Gender    : ${ data[4] }
-            Location  : ${ data[5] }
-            Fee       : ${ data[6] }
-        `);
-    }
-} );
-
-
-
-
-
-// Students of Class 4
-studentInfo.map( ( data, index ) => {
-    if( data[1] == '4' ){
-        console.log(`
-            Name      : ${ data[0] }
-            Class     : ${ data[1] }
-            Roll      : ${ data[2] }
-            Age       : ${ data[3] }
-            Gender    : ${ data[4] }
-            Location  : ${ data[5] }
-            Fee       : ${ data[6] }
-        `);
-    }
-} );
-
-
-
-
-// Students of Class 5
-studentInfo.map( ( data, index ) => {
-    if( data[1] == '5' ){
-        console.log(`
-            Name     : ${ data[0] }
-            Class    : ${ data[1] }
-            Roll     : ${ data[2] }
-            Age      : ${ data[3] }
-            Gender   : ${ data[4] }
-            Location : ${ data[5] }
-            Fee      : ${ data[6] }
-        `);
-    }
-} );
-
-
-
-
-// Location wise student
-studentInfo.map( ( data, index ) => {
-    if( data[5] == 'Banani' ){
-        console.log(`
-            Name       : ${ data[0] }
-            Class      : ${ data[1] }
-            Roll       : ${ data[2] }
-            Age        : ${ data[3] }
-            Gender     : ${ data[4] }
-            Location   : ${ data[5] }
-            Fee        : ${ data[6] }
-        `);
-    }
-} );
-
-
-
-studentInfo.map( ( data, index ) => {
-    if( data[5] == 'Jatrabari' && data[4] == 'male'){
-        console.log(`
-            Name        : ${ data[0] }
-            Class       : ${ data[1] }
-            Roll        : ${ data[2] }
-            Age         : ${ data[3] }
-            Gender      : ${ data[4] }
-            Location    : ${ data[5] }
-            Fee         : ${ data[6] }
-        `);
-    }
-} );
-
-
-
-
-studentInfo.map( ( data, index ) => {
-    if( data[5] == 'Uttara' ){
-        console.log(`
-            Name        : ${ data[0] } 
-            Class       : ${ data[1] } 
-            Roll        : ${ data[2] } 
-            Age         : ${ data[3] } 
-            Gender      : ${ data[4] } 
-            Location    : ${ data[5] } 
-            Fee         : ${ data[6] } 
-        `);
-    }
-} );
+// console.log(`
+// ---------------------------------------
+// Total Admission Fee  = ${total_fee}
+// `);
 
 
 
 
 
 
-// Student between 10 - 25 age
 
-studentInfo.map( ( data, index ) => {
-    
-    if( data[3] > 9 && data[3] <= 25 ){
-        console.log(`
-            Name      : ${ data[0] }
-            Class     : ${ data[1] }
-            Roll      : ${ data[2] }
-            Age       : ${ data[3] }
-            Gender    : ${ data[4] }
-            Location  : ${ data[5] }
-            Fee       : ${ data[6] }
-        `);
-    }
-} );
+
+
+// Find All Female Students
+// for( data of students ){
+//     if( data.gender == 'Female' ){
+//         console.log(`
+//         Name        : ${ data.name }
+//         Roll        : ${ data.roll }
+//         Age         : ${ data.age }
+//         Class Name  : ${ data.classname }
+//         Gender      : ${ data.gender }
+//         Location    : ${ data.location }
+//         Fee         : ${ data.fee }
+//         `);
+//     }
+// }
+
+
+
+
+
+
+
+
+// class wise student result
+// for( data of students ){
+//     if( data.classname == 4 ){
+//         console.log(`
+//             Name        : ${ data.name }
+//             Roll        : ${ data.roll }
+//             Age         : ${ data.age }
+//             Class Name  : ${ data.classname }
+//             Gender      : ${ data.gender }
+//             Location    : ${ data.location }
+//             Fee         : ${ data.fee }
+//         `);
+//     }
+// }
+
+
+
+
+// for( data of students ){
+//     if( data.classname == 3 && data.location == 'Mirpur' ){
+//         console.log(`
+//             Name        : ${ data.name }
+//             Roll        : ${ data.roll }
+//             Age         : ${ data.age }
+//             Class Name  : ${ data.classname }
+//             Gender      : ${ data.gender }
+//             Location    : ${ data.location }
+//             Fee         : ${ data.fee }
+//         `);
+//     }
+// }
+
+
+
+
+// for( data of students ){
+//     if( data.classname == 5 && data.age > 10 && data.age <= 20 ){
+//         console.log(`
+//             Name        : ${ data.name }
+//             Roll        : ${ data.roll }
+//             Age         : ${ data.age }
+//             Class Name  : ${ data.classname }
+//             Gender      : ${ data.gender }
+//             Location    : ${ data.location }
+//             Fee         : ${ data.fee }
+//         `);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// Location wise student result
+// for( data of students ){
+//     if( data.location == 'Banani' ){
+//         console.log(`
+//             Name        : ${ data.name }
+//             Roll        : ${ data.roll }
+//             Age         : ${ data.age }
+//             Class Name  : ${ data.classname }
+//             Gender      : ${ data.gender }
+//             Location    : ${ data.location }
+//             Fee         : ${ data.fee }
+//         `);
+//     }
+// }
+
+
+
+
+// for( data of students ){
+//     if( data.location == 'Uttara' && data.gender == 'Male' ){
+//         console.log(`
+//             Name        : ${ data.name }
+//             Roll        : ${ data.roll }
+//             Age         : ${ data.age }
+//             Class Name  : ${ data.classname }
+//             Gender      : ${ data.gender }
+//             Location    : ${ data.location }
+//             Fee         : ${ data.fee }
+//         `);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+// student between 10 - 25 age
+// for( data of students ){
+//     if( data.age >= 10 && data.age <= 25 ){
+//         console.log(`
+//             Name        : ${ data.name }
+//             Roll        : ${ data.roll }
+//             Age         : ${ data.age }
+//             Class Name  : ${ data.classname }
+//             Gender      : ${ data.gender }
+//             Location    : ${ data.location }
+//             Fee         : ${ data.fee }
+//         `);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
